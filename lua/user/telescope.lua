b1629d39b68401ed4762edf0fa9714dcc47115cd
Quantiux <roy@quantiux.com>
2023-07-30 14:20:04 -0400

    Tweaked telescope.lua

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,3 +1,3 @@
 lvim.builtin.telescope.defaults.layout_config.width = 0.6
 lvim.builtin.telescope.defaults.layout_config.height = 0.5
-lvim.builtin.telescope.defaults.vimgrep_arguments[9] = "--glob=!{.git/,Pipfile.lock,*.csv,*.RData,*.pdf,*.jpg,*.png,*.tif,*.h5,*.joblib,*.dbf,*.prj,*.sbn,*.sbx,*.shp,*.shx}"
+-- lvim.builtin.telescope.defaults.vimgrep_arguments[9] = "--glob=!{.git/,Pipfile.lock,*.csv,*.RData}"
